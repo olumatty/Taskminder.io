2010de6c13aebf45877a2a9a56c371c2748e6c2a
Matthew Adebayo <olumatty@gmail.com>
2024-09-12 04:08:18 +0100

    another commit

--- src/Component/InProgress/InProgress.jsx
+++ src/Component/InProgress/InProgress.jsx
@@ -1,7 +1,7 @@
-import './Inprogress.css'
 import { GrInProgress } from "react-icons/gr";
 import { GoPencil } from "react-icons/go";
 import { HiOutlineTrash } from "react-icons/hi";
+import './Inprogress.css';
 const InProgress = () => {
   return (
     <div className="inProgress-container">
